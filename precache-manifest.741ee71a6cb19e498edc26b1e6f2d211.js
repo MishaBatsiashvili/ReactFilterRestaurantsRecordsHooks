4c63c0816b6d5a3f16e58e22b205d8954a092ee0
misha2016 <mishabatsiashvili@yahoo.com>
2020-06-26 15:23:13 +0400

    Updates

---
+++ precache-manifest.741ee71a6cb19e498edc26b1e6f2d211.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4387cbca5656d0fd6d365da4bab3365",
+    "url": "/ReactFilterRestaurantsRecordsHooks/index.html"
+  },
+  {
+    "revision": "40fd28edc0f943e6c92c",
+    "url": "/ReactFilterRestaurantsRecordsHooks/static/css/main.66c12adf.chunk.css"
+  },
+  {
+    "revision": "3e8706bb81222fe09719",
+    "url": "/ReactFilterRestaurantsRecordsHooks/static/js/2.c1010436.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactFilterRestaurantsRecordsHooks/static/js/2.c1010436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40fd28edc0f943e6c92c",
+    "url": "/ReactFilterRestaurantsRecordsHooks/static/js/main.86ea6daf.chunk.js"
+  },
+  {
+    "revision": "55d843a2dfb75b8f62a7",
+    "url": "/ReactFilterRestaurantsRecordsHooks/static/js/runtime-main.80cd8802.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactFilterRestaurantsRecordsHooks/static/media/logo.5d5d9eef.svg"
+  }
+]);
